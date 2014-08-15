cc3e93ac98fa57fe4cf1373f8f92a8d36e8b58d9
daozhao <daozhao@163.com>
2014-08-15 15:52:10 +0800

    add like objective-C manager file

---
+++ cgoMemory_test.go
@@ -0,0 +1,71 @@
+package gmf
+
+import (
+//	"log"
+	"testing"
+)
+
+type SubData struct {
+	CgoMemoryManage
+	Id   int32
+	Name [16]byte
+}
+
+type Data struct {
+	SubData
+	abc int
+}
+
+var dataIsFree bool = false
+var subDataIsFree bool = false
+
+func (this *Data)Free() {
+//	log.Printf(" Data Free(%p) retainCount=%d",this,this.RetainCount())
+	dataIsFree = true
+}
+
+func (this *SubData)Free() {
+//	log.Printf(" SubData Free(%p) retainCount=%d",this,this.RetainCount())
+	subDataIsFree = true
+}
+
+
+func TestCgoMemory(t *testing.T) {
+
+	var abc *Data
+
+	abc = new(Data)
+	sss := SubData{Id:12}
+
+
+	Retain(abc)
+
+	if ( 2 != abc.RetainCount() ){
+		t.Fatal("has error.")
+	}
+
+	Release(abc)
+
+	if ( 1 != abc.RetainCount() ){
+		t.Fatal("has error.")
+	}
+
+	Release(abc)
+
+	if ( 0 != abc.RetainCount() ){
+		t.Fatal("has error.")
+	}
+
+	if ( ! dataIsFree ) {
+		t.Fatal("Data not run Free.")
+	}
+
+	Release(&sss)
+
+	if ( 0 != sss.RetainCount() ){
+		t.Fatal("has error.")
+	}
+
+	if ( ! subDataIsFree ) {
+		t.Fatal("subData not run Free.")
+	}}
