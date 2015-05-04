b0078eaddda992b7410ef266dfc2e674d9743fd7
Hitoshi Harada <umi.tanuki@gmail.com>
2015-05-04 12:09:03 -0700

    Add test for two new functions.

--- packet_test.go
+++ packet_test.go
@@ -35,3 +35,42 @@ func TestFramesIterator(t *testing.T) {
 	}
 
 }
+
+func TestGetNextFrame(t *testing.T) {
+	inputCtx, err := NewInputCtx(inputSampleFilename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer inputCtx.CloseInputAndRelease()
+
+	for {
+		packet := inputCtx.GetNextPacket()
+		if packet == nil {
+			break
+		}
+		if packet.Size() <= 0 {
+			t.Fatal("Expected size > 0")
+		}
+
+		ist := assert(inputCtx.GetStream(0)).(*Stream)
+
+		f := 0
+		for {
+			frame, err := packet.GetNextFrame(ist.CodecCtx())
+			if frame == nil && err == nil {
+				break
+			}
+			Release(frame)
+			f++
+		}
+
+		if f >= 1 {
+			log.Println(f, "frames decode.")
+			Release(packet)
+			break
+		}
+
+		Release(packet)
+	}
+}
