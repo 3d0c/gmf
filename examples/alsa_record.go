a60dbe8d0bcf453676031e1ef16b78e206a899f9
Marcel Heers <info@heers.it>
2022-01-22 13:47:38 +0200

    fixes alsa recording example

--- examples/alsa_record.go
+++ examples/alsa_record.go
@@ -31,24 +31,25 @@ Sleepy Programmer <hunan@emsym.com>
 package main
 
 import (
+	"fmt"
 	"log"
 
-	. "github.com/3d0c/gmf"
+	"github.com/3d0c/gmf"
 )
 
 func main() {
 	/// input
-	mic, _ := NewInputCtxWithFormatName("default", "alsa")
+	mic, _ := gmf.NewInputCtxWithFormatName("default", "alsa")
 	mic.Dump()
 
-	ast, err := mic.GetBestStream(AVMEDIA_TYPE_AUDIO)
+	ast, err := mic.GetBestStream(gmf.AVMEDIA_TYPE_AUDIO)
 	if err != nil {
 		log.Fatal("failed to find audio stream")
 	}
 	cc := ast.CodecCtx()
 
 	/// fifo
-	fifo := NewAVAudioFifo(cc.SampleFmt(), cc.Channels(), 1024)
+	fifo := gmf.NewAVAudioFifo(cc.SampleFmt(), cc.Channels(), 1024)
 	if fifo == nil {
 		log.Fatal("failed to create audio fifo")
 	}
@@ -56,80 +57,91 @@ func main() {
 	/// output
 	codecName := ""
 	switch cc.SampleFmt() {
-	case AV_SAMPLE_FMT_S16:
+	case gmf.AV_SAMPLE_FMT_S16:
 		codecName = "pcm_s16le"
 	default:
 		log.Fatal("sample format not support")
 	}
-	codec, err := FindEncoder(codecName)
+	codec, err := gmf.FindEncoder(codecName)
 	if err != nil {
 		log.Fatal("find encoder error:", err.Error())
 	}
 
-	occ := NewCodecCtx(codec)
-	if occ == nil {
+	audioEncCtx := gmf.NewCodecCtx(codec)
+	if audioEncCtx == nil {
 		log.Fatal("new output codec context error:", err.Error())
 	}
-	defer Release(occ)
+	defer audioEncCtx.Free()
 
-	occ.SetSampleFmt(cc.SampleFmt()).
-		SetSampleRate(cc.SampleRate()).
-		SetChannels(cc.Channels())
-
-	if err := occ.Open(nil); err != nil {
-		log.Fatal("can't open output codec context", err.Error())
-		return
-	}
-	outputCtx, err := NewOutputCtx("test.wav")
+	outputCtx, err := gmf.NewOutputCtx("test.wav")
 	if err != nil {
 		log.Fatal("new output fail", err.Error())
 		return
 	}
+	defer outputCtx.Free()
 
-	ost := outputCtx.NewStream(codec)
-	if ost == nil {
-		log.Fatal("Unable to create stream for [%s]\n", codec.LongName())
+	audioEncCtx.SetSampleFmt(cc.SampleFmt()).
+		SetSampleRate(cc.SampleRate()).
+		SetChannels(cc.Channels())
+
+	if outputCtx.IsGlobalHeader() {
+		audioEncCtx.SetFlag(gmf.CODEC_FLAG_GLOBAL_HEADER)
 	}
-	defer Release(ost)
 
-	ost.SetCodecCtx(occ)
+	audioStream := outputCtx.NewStream(codec)
+	if audioStream == nil {
+		log.Fatal(fmt.Errorf("unable to create stream for audioEnc [%s]", codec.LongName()))
+	}
+	defer audioStream.Free()
+
+	if err := audioEncCtx.Open(nil); err != nil {
+		log.Fatal("can't open output codec context", err.Error())
+		return
+	}
+	audioStream.DumpContexCodec(audioEncCtx)
+
+	outputCtx.SetStartTime(0)
 
 	if err := outputCtx.WriteHeader(); err != nil {
 		log.Fatal(err.Error())
 	}
 
+	outputCtx.Dump()
+
 	count := 0
 	for packet := range mic.GetNewPackets() {
-		srcFrame, got, err := packet.Decode(ast.CodecCtx())
-		Release(packet)
-		if !got || err != nil {
+		srcFrames, err := ast.CodecCtx().Decode(packet)
+		packet.Free()
+		if err != nil {
 			log.Println("capture audio error:", err)
 			continue
 		}
 
-		wrote := fifo.Write(srcFrame)
-		count += wrote
+		for _, srcFrame := range srcFrames {
+			wrote := fifo.Write(srcFrame)
+			count += wrote
 
-		for fifo.SamplesToRead() >= 1152 {
-			dstFrame := fifo.Read(1152)
-			if dstFrame == nil {
-				continue
-			}
-
-			writePacket, err := dstFrame.Encode(occ)
-			if err == nil {
-				if err := outputCtx.WritePacket(writePacket); err != nil {
-					log.Println("write packet err", err.Error())
+			for fifo.SamplesToRead() >= 1152 {
+				dstFrame := fifo.Read(1152)
+				if dstFrame == nil {
+					continue
 				}
 
-				Release(writePacket)
-			} else {
-				fatal(err)
+				writePacket, err := dstFrame.Encode(audioEncCtx)
+				if err == nil {
+					if err := outputCtx.WritePacket(writePacket); err != nil {
+						log.Println("write packet err", err.Error())
+					}
+
+					writePacket.Free()
+				} else {
+					log.Fatal(err)
+				}
+				dstFrame.Free()
+			}
+			if count > int(cc.SampleRate())*10 {
+				break
 			}
 		}
-		if count > int(cc.SampleRate())*10 {
-			break
-		}
-		Release(srcFrame)
 	}
 }
